0fa5881838e4c26b1a6b5404f2ee791ccd2e8bd8
NatiaBandzeladze <58506962+NatiaBandzeladze@users.noreply.github.com>
2020-02-25 12:41:35 +0400

    burger background

--- javascript/index.js
+++ javascript/index.js
@@ -2,12 +2,24 @@ function toggleBurgerMenu(x) {
     x.classList.toggle("change");
     var navbarList = document.getElementsByClassName("navbar-small");
     var navbar = navbarList[0];
-    if (navbar.style.visibility === "hidden" || !navbar.style.visibility) {
-        navbar.style.visibility = "visible";
-        navbar.style.opacity = "1";
+    if (navbar.style.overflow === "hidden" || !navbar.style.overflow) {
+        navbar.style.overflow = "visible";
+        navbar.style.height = "150px";
     } else {
-        navbar.style.visibility = "hidden";
-        navbar.style.opacity = "0";
+        navbar.style.overflow = "hidden";
+        navbar.style.height = "1px";
+    }
+
+}
+function toggleBurgerMenu(x) {
+    x.classList.toggle("change");
+    var burger = document.getElementsByClassName("burger");
+    var burger = burger[0];
+    if (burger.style.height === "60px" || !burger.style.height) {
+        burger.style.height = "150px";
+    } else {
+        burger.style.height = "60px";
+       
     }
 
 }
