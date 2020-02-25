314e1a0d8a2e0c5f3f470eedcf12d9ad5ec42ca8
NatiaBandzeladze <58506962+NatiaBandzeladze@users.noreply.github.com>
2020-02-25 13:23:10 +0400

    burgern menu transition!

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,17 @@
+// function toggleBurgerMenu(x) {
+//     x.classList.toggle("change");
+//     var navbarList = document.getElementsByClassName("navbar-small");
+//     var navbar = navbarList[0];
+//     if (navbar.style.overflow === "hidden" || !navbar.style.overflow) {
+//         navbar.style.overflow = "visible";
+//         navbar.style.height = "150px";
+//     } else {
+//         navbar.style.overflow = "hidden";
+//         navbar.style.height = "1px";
+//     }
+
+// }
+
 function toggleBurgerMenu(x) {
     x.classList.toggle("change");
     var navbarList = document.getElementsByClassName("navbar-small");
@@ -9,10 +23,6 @@ function toggleBurgerMenu(x) {
         navbar.style.overflow = "hidden";
         navbar.style.height = "1px";
     }
-
-}
-function toggleBurgerMenu(x) {
-    x.classList.toggle("change");
     var burger = document.getElementsByClassName("burger");
     var burger = burger[0];
     if (burger.style.height === "60px" || !burger.style.height) {
@@ -23,3 +33,18 @@ function toggleBurgerMenu(x) {
     }
 
 }
+
+
+
+// function toggleBurgerMenu(x) {
+//     x.classList.toggle("change");
+//     var burger = document.getElementsByClassName("burger");
+//     var burger = burger[0];
+//     if (burger.style.height === "60px" || !burger.style.height) {
+//         burger.style.height = "150px";
+//     } else {
+//         burger.style.height = "60px";
+       
+//     }
+
+// }
