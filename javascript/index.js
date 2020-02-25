f3ded0f000899c679ee5b21ed7053e8c7a8ef373
NatiaBandzeladze <58506962+NatiaBandzeladze@users.noreply.github.com>
2020-02-25 11:36:07 +0400

    burger click

--- javascript/index.js
+++ javascript/index.js
@@ -2,10 +2,12 @@ function toggleBurgerMenu(x) {
     x.classList.toggle("change");
     var navbarList = document.getElementsByClassName("navbar-small");
     var navbar = navbarList[0];
-    if (navbar.style.display === "none" || !navbar.style.display) {
-        navbar.style.display = "block";
-      } else {
-        navbar.style.display = "none";
-      }
+    if (navbar.style.visibility === "hidden" || !navbar.style.visibility) {
+        navbar.style.visibility = "visible";
+        navbar.style.opacity = "1";
+    } else {
+        navbar.style.visibility = "hidden";
+        navbar.style.opacity = "0";
+    }
 
 }
